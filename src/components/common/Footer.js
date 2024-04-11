cb144c2f06c0404b288e967396cb88a674e0dfd8
Lâm Xuân Nguyên <100450698+imnguin@users.noreply.github.com>
2024-04-11 16:26:16 +0700

    commit

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,82 +2,98 @@ import React from "react";
 
 const Footer = (props) => {
     return (
-        <footer classNameName="footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12 col-md-12 col-lg-4  menu-footer-one">
-                        <a href="#" className="logo-footer">
-                            <img src={require('../../assets/images/nailsspalogo.png')} alt="" />
-                        </a>
-                        <ul className="footer-list no-bullets">
-                            <li>
-                                <span className="icon">
-                                    <i className="ti-home" aria-hidden="true"></i>
-                                </span>
-                                <p>Address: S1515 SE 17th St #109
-                                    Fort Lauderdale, Florida 33316</p>
-                            </li>
-                            <li>
-                                <span className="icon">
-                                    <i className="fa fa-volume-control-phone" aria-hidden="true"></i>
-                                </span>
-                                <p>
-                                    Phone:
-                                    <a href="tel:(+84)934 323 882">(469) 919-0777</a>
-                                </p>
-                            </li>
-                            <li>
-                                <span className="icon">
-                                    <i className="ti-email" aria-hidden="true"></i>
-                                </span>
-                                <p>
-                                    Email :
-                                    <a href="mailto: support@suplo.vn">doradonailbarforney@gmail.com</a>
-                                </p>
-                            </li>
-                            <li>
-                                <span className="icon">
-                                    <i className="ti-world" aria-hidden="true"></i>
-                                </span>
-                                <p>
-                                    Website :
-                                    <a href="https://doradonailbarforney.com">doradonailbarforney.com</a>
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="col-sm-12 col-md-12 col-lg-4 menu-footer menu-footer-two">
-                        <h3 className="menu-link">Location</h3>
-                        <div className="footer-list content">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14331.68875213!2d-80.1267145!3d26.1014586!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x20dd4fa1dc5a0c79!2sUltimate+Nail+Spa!5e0!3m2!1sen!2s!4v1522055244983"
-                                width="300" height="250">
-                                <p>Your browser does not support iframes.</p>
-                            </iframe>
-                        </div>
-                    </div>
-
-                    <div className="col-sm-12 col-md-12 col-lg-4 menu-footer menu-footer-three">
-                        <h3 className="menu-link">Hours of Operation</h3>
-                        <div className="footer-list content">
-                            <ul className="homehours">
-                                <li>Mon-Sat: 10:00 am - 7:30 pm </li>
-                                <li>Sun: 11:00 am - 5:00 pm </li>
+        <>
+            <footer classNameName="footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-12 col-lg-4  menu-footer-one">
+                            <a href="#" className="logo-footer">
+                                <img src={require('../../assets/images/nailsspalogo.png')} alt="" />
+                            </a>
+                            <ul className="footer-list no-bullets">
+                                <li>
+                                    <span className="icon">
+                                        <i className="ti-home" aria-hidden="true"></i>
+                                    </span>
+                                    <p>Address: S1515 SE 17th St #109
+                                        Fort Lauderdale, Florida 33316</p>
+                                </li>
+                                <li>
+                                    <span className="icon">
+                                        <i className="fa fa-volume-control-phone" aria-hidden="true"></i>
+                                    </span>
+                                    <p>
+                                        Phone:
+                                        <a href="tel:(+84)934 323 882">(469) 919-0777</a>
+                                    </p>
+                                </li>
+                                <li>
+                                    <span className="icon">
+                                        <i className="ti-email" aria-hidden="true"></i>
+                                    </span>
+                                    <p>
+                                        Email :
+                                        <a href="mailto: support@suplo.vn">doradonailbarforney@gmail.com</a>
+                                    </p>
+                                </li>
+                                <li>
+                                    <span className="icon">
+                                        <i className="ti-world" aria-hidden="true"></i>
+                                    </span>
+                                    <p>
+                                        Website :
+                                        <a href="https://doradonailbarforney.com">doradonailbarforney.com</a>
+                                    </p>
+                                </li>
                             </ul>
-                            <div className="weaccept">
-                                <h3>We Accept:</h3>
-                                <div className="lstbank">
-                                    <img src={require('../../assets/images/social/visa.png')} alt="visa" />
-                                    <img src={require('../../assets/images/social/master.png')} alt="master" />
-                                    <img src={require('../../assets/images/social/discover.png')} alt="discover" />
+                        </div>
+
+                        <div className="col-sm-12 col-md-12 col-lg-4 menu-footer menu-footer-two">
+                            <h3 className="menu-link">Location</h3>
+                            <div className="footer-list content">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14331.68875213!2d-80.1267145!3d26.1014586!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x20dd4fa1dc5a0c79!2sUltimate+Nail+Spa!5e0!3m2!1sen!2s!4v1522055244983"
+                                    width="300" height="250">
+                                    <p>Your browser does not support iframes.</p>
+                                </iframe>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-12 col-md-12 col-lg-4 menu-footer menu-footer-three">
+                            <h3 className="menu-link">Hours of Operation</h3>
+                            <div className="footer-list content">
+                                <ul className="homehours">
+                                    <li>Mon-Sat: 10:00 am - 7:30 pm </li>
+                                    <li>Sun: 11:00 am - 5:00 pm </li>
+                                </ul>
+                                <div className="weaccept">
+                                    <h3>We Accept:</h3>
+                                    <div className="lstbank">
+                                        <img src={require('../../assets/images/social/visa.png')} alt="visa" />
+                                        <img src={require('../../assets/images/social/master.png')} alt="master" />
+                                        <img src={require('../../assets/images/social/discover.png')} alt="discover" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+
+            <section id="footer-info" className="footer-info">
+                <div className="footer-bottom">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <div className="footer-bottom-contact">
+                                    Copyrights © 2024 by <a target="_blank" href="#"> Dorado Nail Bar.</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 }
 export default Footer;
--- src/views/Home.js
+++ src/views/Home.js
@@ -88,6 +88,315 @@ const Home = (props) => {
                             </ul>
                         </div>
                     </div>
+
+                    <div className="borderservices">
+                        <img src={require('../assets/images/bg_services.png')} alt="" />
+                    </div>
+
+                    <div className="row servicesbox">
+                        <div className="col-12 col-md-7 col-lg-8 servicescontent">
+                            <div className="servicestitle">Pedicure</div>
+                            <ul className="lstservice">
+                                <li className="item">
+                                    Basic Pedicure
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Honey Cappucino
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Wild Orchid Gardenia
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Rosemary Vanilla
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Pomegranate & Figs
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Tangerine or Green Tea Jelly
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Pina Colada
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Cooling Spearmint
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Organic Milk & Honey
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Organic Green Tea$4
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    OPI Lemon
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Honey Cucumber Mint
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Champagne & Rose
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Coconut Lime Butter
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Pumpkin Spice
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Chocolate Butter Creme
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Nu Skin Pedicure
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Add Hot Stone
+                                    <div className="price">$12</div>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div className="col-12 col-md-5 col-lg-4 hidden_m">
+                            <div className="servicesimg">
+                                <img src={require('../assets/images/ourservice/3.jpg')} alt="manicure pedicure" />
+                                <img src={require('../assets/images/ourservice/03.jpg')} alt="manicure pedicure" className="servicesimg2" />
+                            </div>
+                            <div className="servicesimg">
+                                <img src={require('../assets/images/ourservice/4.jpg')} alt="manicure pedicure" />
+                                <img src={require('../assets/images/ourservice/04.jpg')} alt="manicure pedicure" className="servicesimg2" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="borderservices">
+                        <img src={require('../assets/images/bg_services.png')} alt="" />
+                    </div>
+
+                    <div className="row servicesbox">
+                        <div className="col-12 col-md-5 col-lg-4 hidden_m">
+                            <div className="servicesimg">
+                                <img src={require('../assets/images/ourservice/5.jpg')} alt="manicure pedicure" />
+                                <img src={require('../assets/images/ourservice/05.jpg')} alt="manicure pedicure" className="servicesimg2" />
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-7 col-lg-8 servicescontent">
+                            <div className="servicestitle">Herbal Spa</div>
+
+                            <ul className="lstservice">
+                                <li className="item">
+                                    One Herb Treatment
+                                    <div className="price">$12</div>
+                                    <div className="note">This Manicure includes nails trim and shaping, buffing, cuticle grooming
+                                        and conditioning, lotion massage and color of your choice or buff to perfect shine.
+                                    </div>
+                                </li>
+                                <li className="item">
+                                    Two Herb Treatment
+                                    <div className="price">$12</div>
+                                    <div className="note">This Manicure includes nails trim and shaping, buffing, cuticle grooming
+                                        and conditioning, lotion massage and color of your choice or buff to perfect shine.
+                                    </div>
+                                </li>
+                                <li className="item">
+                                    Add Hotstone
+                                    <div className="price">$12</div>
+                                    <div className="note">This Manicure includes nails trim and shaping, buffing, cuticle grooming
+                                        and conditioning, lotion massage and color of your choice or buff to perfect shine.
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div className="borderservices">
+                        <img src={require('../assets/images/bg_services.png')} alt="" />
+                    </div>
+
+                    <div className="row servicesbox">
+                        <div className="col-12 col-md-7 col-lg-8 servicescontent">
+                            <div className="servicestitle">Children</div>
+                            <ul className="lstservice">
+                                <li className="item">
+                                    Polish Change
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Kids Pedicure
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Manicure
+                                    <div className="price">$12</div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-12 col-md-5 col-lg-4 hidden_m">
+                            <div className="servicesimg">
+                                <img src={require('../assets/images/ourservice/6.jpg')} alt="manicure pedicure" />
+                                <img src={require('../assets/images/ourservice/06.jpg')} alt="manicure pedicure" className="servicesimg2" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="borderservices">
+                        <img src={require('../assets/images/bg_services.png')} alt="" />
+                    </div>
+
+                    <div className="row servicesbox">
+                        <div className="col-12 col-md-5 col-lg-4 hidden_m">
+                            <div className="servicesimg">
+                                <img src={require('../assets/images/ourservice/7.jpg')} alt="manicure pedicure" />
+                                <img src={require('../assets/images/ourservice/07.jpg')} alt="manicure pedicure" className="servicesimg2" />
+                            </div>
+                            <div className="servicesimg">
+                                <img src={require('../assets/images/ourservice/8.jpg')} alt="manicure pedicure" />
+                                <img src={require('../assets/images/ourservice/08.jpg')} alt="manicure pedicure" className="servicesimg2" />
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-7 col-lg-8 servicescontent">
+                            <div className="servicestitle">Waxing</div>
+                            <ul className="lstservice">
+                                <li className="item">
+                                    Eyebrows
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Lip
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Chin
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Neck
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Cheek / Sideburn
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Underarm
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Half Arm
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Full Arm
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Half Leg
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Full Leg
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Bikini
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Brazilian
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Half Back
+                                    <div className="price">$12</div>
+                                </li>
+                                <li className="item">
+                                    Full Back
+                                    <div className="price">$12</div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="borderservices">
+                        <img src={require('../assets/images/bg_services.png')} alt="" />
+                    </div>
+
+                    <div class="row servicesbox">
+                        <div class="col-12 col-md-7 col-lg-8 servicescontent">
+                            <div class="servicestitle">Eyebrow Tint</div>
+                            <ul class="lstservice">
+                                <li class="item">
+                                    Eyebrow Wax & Tint
+                                    <div class="price">$12</div>
+                                </li>
+                                <li class="item">
+                                    Facial Mini
+                                    <div class="price">$12</div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-md-5 col-lg-4 hidden_m">
+                            <div class="servicesimg">
+                                <img src={require('../assets/images/ourservice/9.jpg')} alt="manicure pedicure" />
+                                <img src={require('../assets/images/ourservice/9.jpg')} alt="manicure pedicure" class="servicesimg2" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="borderservices">
+                        <img src={require('../assets/images/bg_services.png')} alt="" />
+                    </div>
+
+                    <div class="row servicesbox">
+                        <div class="col-12 col-md-5 col-lg-4 hidden_m">
+                            <div class="servicesimg">
+                                <img src={require('../assets/images/ourservice/10.jpg')} alt="manicure pedicure" />
+                                <img src={require('../assets/images/ourservice/010.jpg')} alt="manicure pedicure" class="servicesimg2" />
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-7 col-lg-8 servicescontent">
+                            <div class="servicestitle">Eyelash</div>
+                            <ul class="lstservice">
+                                <li class="item">
+                                    Individual (classic)
+                                    <div class="price">$12</div>
+                                </li>
+                                <li class="item">
+                                    Fill
+                                    <div class="price">$12</div>
+                                </li>
+                                <li class="item">
+                                    Volume
+                                    <div class="price">$12</div>
+                                </li>
+                                <li class="item">
+                                    Fill
+                                    <div class="price">$12</div>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div className="borderservices">
+                        <img src={require('../assets/images/bg_services.png')} alt="" />
+                    </div>
                 </div>
             </section>
         </>
